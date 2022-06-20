9851f09e70441667b2311d2cbf8975b4d0f7abb2
Matt Bramlage <mbramlage@gmail.com>
2022-06-20 13:58:41 -0700

    Works as installation from relative directory

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='python_logging_base',
+    version='0.1.1',    
+    description='Matt\'s base Python logging setup',
+    url='https://github.com/praecipula/python_logging_base',
+    author='Matt Bramlage',
+    license='MIT',
+    packages=['python_logging_base'],
+    install_requires=['pyyaml'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Utility/Personal',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3',
+    ],
+)
