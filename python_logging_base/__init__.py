1a3f1b4ba405c63c55a5e889e9e7c2ea6846635e
Matt Bramlage <mbramlage@gmail.com>
2024-03-01 12:08:50 -0800

    Whoops, message property is deprecated; just use str of exc

--- python_logging_base/__init__.py
+++ python_logging_base/__init__.py
@@ -454,7 +454,7 @@ def ASSERT(condition_that_should_be_true=False, exc_or_message="Unspecified", de
         return True
     if isinstance(exc_or_message, Exception):
         exc = exc_or_message
-        ASSERT_LOG.critical(f"Failed exception: {type(exc)}, {exc.message}")
+        ASSERT_LOG.critical(f"Failed exception: {type(exc)}, {str(exc)}")
         if debug_on_fail:
             import pdb; pdb.set_trace()
         raise exc
